973793efa0d54cbc9e431342cb59a132490ca200
innocent <innocentgodwin580@yahoo.com>
2025-09-03 14:17:28 +0930

    11

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from cicd github hellO-inno-12345678 ')
+        'body': json.dumps('Hello from cicd github hellO-inno-123456789 ')
     }
